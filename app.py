ca6e82a1710e90a63dc749480e5d1027d9ce06a4
Gintoki-G <bambi110723@naver.com>
2025-11-24 13:48:40 +0900

    dashboard

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session
+
 app = Flask(__name__)
+app.secret_key = "test"
 
-@app.route('/')
-def index():
-    return "Hello, Secure Flask!"
+# 임시 사용자 데이터
+USERS = {
+    "admin": {"password": "1234", "balance": 500000}
+}
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+#@app.route('/', methods=['GET', 'POST'])
+# = 루트 URL에서 GET과 POST 요청을 처리하는 기능을 만들겠다
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = USERS.get(username)
+        if user and user["password"] == password:
+            session['username'] = username
+            return redirect(url_for("dashboard"))
+        else:
+            return "로그인 실패!"
+
+    return render_template("login.html")
+
+@app.route('/dashboard')
+def dashboard():
+    if(username not in session) : 
+        return redirect(url_for('login'))
+    username = session['username']
+    balance = USERS[username]["balance"]
+    return render_template("dashboard.html", username=username, balance=balance)
