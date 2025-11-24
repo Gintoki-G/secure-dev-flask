c4a55ea01b4f2977d0f1bd5c875b0dccd128747c
Gintoki-G <bambi110723@naver.com>
2025-11-24 15:55:35 +0900

    std

--- app.py
+++ app.py
@@ -10,13 +10,18 @@ app.secret_key = "test"
 USERS = {
     "admin": {"password": "1234", "balance": 500000}
 }
-
+# 라우팅 : 웹에서 url과 처리 함수를 연결
+# 서버는 어떤 코드를 실행해야하는 지 알아야 한다.
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
+        #사용자가 입력 후 제출하면 서버가 POST 데이터를 받고
+        #request.form 딕셔너리에 담깁니다.
         username = request.form['username']
         password = request.form['password']
 
+        #이제 서버가 사용자 정보를 저장한 데이터베이스(혹은 딕셔너리)에서
+        #실제 계정 정보를 가져오는 것
         user = USERS.get(username)
         if user and user["password"] == password:
             session['username'] = username
@@ -45,11 +50,18 @@ def transfer():
 
     if request.method == 'POST':
         amount = int(request.form['amount'])
-        account_number =request.form[account_number] #계좌 번호 내역 추가
+        account_number = request.form['account_number']  # 수정됨
+
         USERS[username]["balance"] -= amount
+
+        # 계좌번호와 금액을 세션에 저장
+        session['last_account'] = account_number
+        session['last_amount'] = amount
+
         return redirect(url_for("accountsend"))
 
     return render_template("transfer.html", username=username)
+
 
 @app.route('/accountsend')
 def accountsend():
